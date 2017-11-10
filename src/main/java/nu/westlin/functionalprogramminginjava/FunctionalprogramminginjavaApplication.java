b3e8e326ba9e15a54ca8049bef445ae9d07d6f32
Peter Vestlin <peter.vestlin@lm.se>
2017-11-10 15:27:55 +0100

    Setting up new project

---
+++ src/main/java/nu/westlin/functionalprogramminginjava/FunctionalprogramminginjavaApplication.java
@@ -0,0 +1,12 @@
+package nu.westlin.functionalprogramminginjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionalprogramminginjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunctionalprogramminginjavaApplication.class, args);
+	}
+}
